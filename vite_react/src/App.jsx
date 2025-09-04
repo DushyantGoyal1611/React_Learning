1bebd27738da12ecf311444532e4238f1d2f9501
Dushyant Goyal <goyaldushyant1611.com>
2025-09-04 16:05:47 +0530

    initial Commit

---
+++ vite_react/src/App.jsx
@@ -0,0 +1,13 @@
+import MSG from "./Demo"
+
+function App() {
+  return (
+    <>
+      <MSG/>
+      <h2>The Message is:</h2>
+      <h3>Hello Dushyant, Nice meeting you</h3>
+    </>
+  )
+}
+
+export default App
---
+++ vite_react/src/Demo.jsx
@@ -0,0 +1,7 @@
+function MSG(){
+    return (
+        <h3>Message is displayed</h3>
+    )
+}
+
+export default MSG
